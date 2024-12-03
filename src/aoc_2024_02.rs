17b0bbb3e839ac4548019614ede370f39e3ff4e0
Daniel Boros <dancixx@gmail.com>
2024-12-03 01:06:35 +0100

    feat: add day 2. b

--- src/aoc_2024_02.rs
+++ src/aoc_2024_02.rs
@@ -6,7 +6,8 @@ pub fn solution() {
 
     // A.
     let mut safe_reports_a = 0;
-    for line in aoc_2024_a.lines() {
+    let mut safe_reports = Vec::<usize>::new();
+    for (idx, line) in aoc_2024_a.lines().enumerate() {
         let numbers = line
             .split_whitespace()
             .map(|u| u.parse::<i8>().unwrap())
@@ -16,10 +17,43 @@ pub fn solution() {
 
         if numbers.is_sorted_by(ascending) || numbers.is_sorted_by(descending) {
             safe_reports_a = safe_reports_a + 1;
+
+            // collect the indexes of the unsafe reports
+            safe_reports.push(idx);
         }
     }
     println!("Number of safe reports is: {}", safe_reports_a);
 
     // B.
-  
+    let asc = |levels: &[i8]| {
+        levels
+            .windows(2)
+            .all(|w| w[1] > w[0] && (1..=3).contains(&(w[1] - w[0])))
+    };
+    let desc = |levels: &[i8]| {
+        levels
+            .windows(2)
+            .all(|w| w[1] < w[0] && (1..=3).contains(&(w[0] - w[1])))
+    };
+
+    let is_safe = |levels: &[i8]| -> bool {
+        (asc(levels) || desc(levels))
+            || (0..levels.len()).any(|i| {
+                let mut levels = levels.to_vec();
+                levels.remove(i);
+                asc(&levels) || desc(&levels)
+            })
+    };
+
+    let levels = aoc_2024_a
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|u| u.parse::<i8>().unwrap())
+                .collect::<Vec<i8>>()
+        })
+        .collect::<Vec<Vec<i8>>>();
+
+    let count = levels.into_iter().filter(|l| is_safe(l)).count();
+    println!("Number of safe reports is: {}", count);
 }
