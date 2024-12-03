4ba4faa3c8322ea86c23ddd134edfbe6d1207c3e
zeldan <zelei.dan@gmail.com>
2024-12-03 20:09:10 +0100

    feat_ add day 3. b

--- src/aoc_2024_03.rs
+++ src/aoc_2024_03.rs
@@ -3,20 +3,34 @@ use std::fs;
 use regex::Regex;
 
 pub fn solution() {
-    let aoc_2024_a =
+    let aoc_2024_03 =
         fs::read_to_string("./src/aoc_2024_03.txt").expect("Something went wrong reading the file");
 
     // A.
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    let caps = re.captures_iter(&aoc_2024_a);
+    let matching_regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let caps = matching_regex.captures_iter(&aoc_2024_03);
     
     let mut sum = 0;
     for cap in caps {
-        println!("{} - {}", cap.get(1).unwrap().as_str(), cap.get(2).unwrap().as_str());
         let first_value = cap.get(1).unwrap().as_str().parse::<i32>().unwrap();
         let second_value = cap.get(2).unwrap().as_str().parse::<i32>().unwrap();
         sum = sum + (first_value * second_value);
     }
+    println!("Result: {}", sum);
+
+    // B.
+    let text_without_new_lines = aoc_2024_03.replace("\n", "to");
+    let regex_do_not_do = Regex::new(r"don't.*?(do\(\)|$)").unwrap();
+    let text_without_do_not_do = regex_do_not_do.replace_all(&text_without_new_lines, "");
+    let matching_regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let caps = matching_regex.captures_iter(&text_without_do_not_do);
+    let mut sum = 0;
+    for cap in caps {
+        let first_value = cap.get(1).unwrap().as_str().parse::<i32>().unwrap();
+        let second_value = cap.get(2).unwrap().as_str().parse::<i32>().unwrap();
+        sum = sum + (first_value * second_value);
+    }
+
     println!("Result: {}", sum);
 
 }
